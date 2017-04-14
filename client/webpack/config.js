87fe3df5db8e29cf2124d3cf9d4bdf857d193fc9
Karl <karlding@users.noreply.github.com>
2017-04-14 16:00:11 -0400

    Holy it works

---
+++ client/webpack/config.js
@@ -0,0 +1,16 @@
+var path = require('path')
+
+module.exports = {
+  entry: {
+    app: ['./css/app.scss', './js/app.ts']
+  },
+  port: 3003,
+  html: true,
+  browsers: ['last 2 versions', 'ie > 8'],
+  assets_url: '/',
+  stylelint: './css/**/*.scss',
+  assets_path: path.join(__dirname, '../dist/'),
+  refresh: ['./index.html'],
+  historyApiFallback: false,
+  debug: process.env.NODE_ENV === 'development'
+}
---
+++ client/webpack/webpack.prod.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const webpack = require('webpack')
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const AssetsPlugin = require('assets-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+const webpack_base = require('./webpack.base')
+const config = require('./config')
+
+webpack_base.plugins.push(
+  new ProgressBarPlugin(),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    },
+    comments: false
+  }),
+  new AssetsPlugin({
+    filename: path.join('dist', 'assets.json')
+  }),
+  new CopyWebpackPlugin([
+    {
+      from: 'img',
+      to: 'img'
+    }
+  ])
+);
+
+module.exports = webpack_base;
