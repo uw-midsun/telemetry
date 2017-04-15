bc9cbde80f4181a3836c30e5fd4cf9f202acfa27
Karl <karlding@users.noreply.github.com>
2017-04-14 22:06:37 -0400

    linting

--- client/webpack/server-client.js
+++ client/webpack/server-client.js
@@ -1,137 +1,152 @@
 'use strict'
 
+var processUpdate = require('webpack-hot-middleware/process-update')
 var config = require('./config')
 
 // From https://raw.githubusercontent.com/glenjamin/webpack-hot-middleware/master/client.js
 var options = {
-  path: "http://localhost:" + config.port + "/__webpack_hmr",
+  path: 'http://localhost:' + config.port + '/__webpack_hmr',
   timeout: 20 * 1000,
   overlay: true,
   reload: true,
   log: false,
-  warn: false,
-};
+  warn: false
+}
 
 if (typeof window === 'undefined') {
   // do nothing
 } else if (typeof window.EventSource === 'undefined') {
   console.warn(
-    "webpack-hot-middleware's client requires EventSource to work. " +
-    "You should include a polyfill if you want to support this browser: " +
-    "https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events#Tools"
-  );
+    'webpack-hot-middleware\'s client requires EventSource to work. ' +
+    'You should include a polyfill if you want to support this browser: ' +
+    'https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events#Tools'
+  )
 } else {
-  connect(window.EventSource);
+  connect(window.EventSource)
 }
 
-function connect(EventSource) {
-  var source = new EventSource(options.path);
-  var lastActivity = new Date();
+function connect (EventSource) {
+  var source = new EventSource(options.path)
+  var lastActivity = new Date()
 
-  source.onopen = handleOnline;
-  source.onmessage = handleMessage;
-  source.onerror = handleDisconnect;
+  source.onopen = handleOnline
+  source.onmessage = handleMessage
+  source.onerror = handleDisconnect
 
-  var timer = setInterval(function() {
+  var timer = setInterval(function () {
     if ((new Date() - lastActivity) > options.timeout) {
-      handleDisconnect();
+      handleDisconnect()
     }
-  }, options.timeout / 2);
+  }, options.timeout / 2)
 
-  function handleOnline() {
-    if (options.log) console.log("[HMR] connected");
-    lastActivity = new Date();
+  function handleOnline () {
+    if (options.log) {
+      console.log('[HMR] connected')
+    }
+    lastActivity = new Date()
   }
 
-  function handleMessage(event) {
-    lastActivity = new Date();
-    if (event.data == "\uD83D\uDC93") {
-      return;
+  function handleMessage (event) {
+    lastActivity = new Date()
+    if (event.data === '\uD83D\uDC93') {
+      return
     }
     try {
-      processMessage(JSON.parse(event.data));
+      processMessage(JSON.parse(event.data))
     } catch (ex) {
       if (options.warn) {
-        console.warn("Invalid HMR message: " + event.data + "\n" + ex);
+        console.warn('Invalid HMR message: ' + event.data + '\n' + ex)
       }
     }
   }
 
-  function handleDisconnect() {
-    clearInterval(timer);
-    source.close();
-    setTimeout(function() { connect(EventSource); }, options.timeout);
-  }
+  function handleDisconnect () {
+    clearInterval(timer)
+    source.close()
 
+    setTimeout(function () {
+      connect(EventSource)
+    }, options.timeout)
+  }
 }
 
-var reporter;
+var reporter
 // the reporter needs to be a singleton on the page
 // in case the client is being used by mutliple bundles
 // we only want to report once.
 // all the errors will go to all clients
-var singletonKey = '__webpack_hot_middleware_reporter__';
+var singletonKey = '__webpack_hot_middleware_reporter__'
 if (typeof window !== 'undefined' && !window[singletonKey]) {
-  reporter = window[singletonKey] = createReporter();
+  reporter = window[singletonKey] = createReporter()
 }
 
-function createReporter() {
-  var strip = require('strip-ansi');
+function createReporter () {
+  var strip = require('strip-ansi')
 
-  var overlay;
+  var overlay
   if (typeof document !== 'undefined' && options.overlay) {
-    overlay = require('webpack-hot-middleware/client-overlay');
+    overlay = require('webpack-hot-middleware/client-overlay')
   }
 
   return {
-    problems: function(type, obj) {
+    problems: function (type, obj) {
       if (options.warn) {
-        console.warn("[HMR] bundle has " + type + ":");
-        obj[type].forEach(function(msg) {
-          console.warn("[HMR] " + strip(msg));
-        });
+        console.warn('[HMR] bundle has ' + type + ':')
+
+        obj[type].forEach(function (msg) {
+          console.warn('[HMR] ' + strip(msg))
+        })
       }
-      if (overlay && type !== 'warnings') overlay.showProblems(type, obj[type]);
+      if (overlay && type !== 'warnings') {
+        overlay.showProblems(type, obj[type])
+      }
     },
-    success: function() {
-      if (overlay) overlay.clear();
+    success: function () {
+      if (overlay) {
+        overlay.clear()
+      }
     },
-    useCustomOverlay: function(customOverlay) {
-      overlay = customOverlay;
+    useCustomOverlay: function (customOverlay) {
+      overlay = customOverlay
     }
-  };
+  }
 }
 
-var processUpdate = require('webpack-hot-middleware/process-update');
+var customHandler
+var subscribeAllHandler
 
-var customHandler;
-var subscribeAllHandler;
-function processMessage(obj) {
-  if (obj.action == "building") {
-    if (options.log) console.log("[HMR] bundle rebuilding");
-  } else if (obj.action == "built") {
+function processMessage (obj) {
+  if (obj.action === 'building') {
+    if (options.log) {
+      console.log('[HMR] bundle rebuilding')
+    }
+  } else if (obj.action === 'built') {
     if (options.log) {
       console.log(
-        "[HMR] bundle " + (obj.name ? obj.name + " " : "") +
-        "rebuilt in " + obj.time + "ms"
-      );
+        '[HMR] bundle ' + (obj.name ? obj.name + ' ' : '') +
+        'rebuilt in ' + obj.time + 'ms'
+      )
     }
     if (obj.errors.length > 0) {
-      if (reporter) reporter.problems('errors', obj);
+      if (reporter) {
+        reporter.problems('errors', obj)
+      }
     } else {
       if (reporter) {
-        if (obj.warnings.length > 0) reporter.problems('warnings', obj);
-        reporter.success();
+        if (obj.warnings.length > 0) {
+          reporter.problems('warnings', obj)
+        }
+        reporter.success()
       }
 
-      processUpdate(obj.hash, obj.modules, options);
+      processUpdate(obj.hash, obj.modules, options)
     }
   } else if (customHandler) {
-    customHandler(obj);
+    customHandler(obj)
   }
 
   if (subscribeAllHandler) {
-    subscribeAllHandler(obj);
+    subscribeAllHandler(obj)
   }
 }
 
