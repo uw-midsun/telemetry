f6a50ce5a99166b4b8e51fd5790c3270339c294e
ckitagawa <calderk2010@gmail.com>
2018-06-28 18:51:14 -0400

    ELEC-494: Improve README and autostart

--- driver_display_startup.sh
+++ driver_display_startup.sh
@@ -1,11 +1,14 @@
 #! /bin/sh
 
+# Kill any old instances of telemetry.
+pkill telemetry
+
 # Start serving the website. Use 'nice' so the process plays nice with
 # scheduling priority otherwise it can overrun.
 nice ./bin/telemetry start --tty=/dev/ttyAMA0 --db=can.db \
   --schema=can_messages.asciipb &
 
-# Start a very minimal version of Chromium in fullscreen
+# Start a very minimal version of Chromium in fullscreen.
 chromium-browser --kiosk --incognito --noerrdialogs --disable-infobars \
   http://localhost:8080/driver_display.html &
 
