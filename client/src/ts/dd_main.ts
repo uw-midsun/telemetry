5b510fa3c4d27e8cb5c6102cf73d66158111f52a
Calder Kitagawa <calderk2010@gmail.com>
2018-06-27 21:21:48 -0400

    ELEC-491: Fix precision (#23)
    The current precision is not sufficient. I've changes all of them to be 3-4 digits which should show up reasonably in most cases. I've also added a conversion for current from uA to A.

--- client/src/ts/dd_main.ts
+++ client/src/ts/dd_main.ts
@@ -248,21 +248,21 @@ ws.onmessage = (event) => {
       break;
     case canDefs.CanMessage.CAN_MESSAGE_AUX_DCDC_VC:
       document.getElementById('aux-current').innerHTML =
-          (msg.data.aux_current / 1000).toPrecision(2) + ' mA';
+          (msg.data.aux_current / 1000).toFixed(2) + ' mA';
       document.getElementById('aux-voltage').innerHTML =
-          (msg.data.aux_voltage / 1000).toPrecision(2) + ' V';
+          (msg.data.aux_voltage / 1000).toFixed(2) + ' V';
       document.getElementById('dcdc-current').innerHTML =
-          (msg.data.dcdc_current / 1000).toPrecision(2) + ' mA';
+          (msg.data.dcdc_current / 1000).toFixed(2) + ' mA';
       document.getElementById('dcdc-voltage').innerHTML =
-          (msg.data.dcdc_voltage / 1000).toPrecision(2) + ' V';
+          (msg.data.dcdc_voltage / 1000).toFixed(2) + ' V';
       break;
     case canDefs.CanMessage.CAN_MESSAGE_BATTERY_AGGREGATE_VC:
-      const converted_current = msg.data.current;
+      const converted_current = msg.data.current / 1000000;
       const converted_voltage = msg.data.voltage / 10000;
       document.getElementById('mp-current').innerHTML =
-          (converted_current).toPrecision(2) + ' A';
+          (converted_current).toFixed(1) + ' A';
       document.getElementById('mp-voltage').innerHTML =
-          (converted_voltage).toPrecision(2) + ' V';
+          (converted_voltage).toFixed(1) + ' V';
       power_consumption_graph.addData(
           {x: msg.timestamp, y: converted_current * converted_voltage});
       consumptionReadout.value(power);
